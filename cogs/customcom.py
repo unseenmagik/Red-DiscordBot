cc0200210359c8e53f46f11609fa0b670f539ee6
Will Tekulve <tekulvwc@mail.uc.edu>
2016-02-21 13:02:38 -0500

    Add alias for custom commands Add runescape cog Add chat text formatting utility
    Signed-off-by: Will Tekulve <tekulvwc@mail.uc.edu>

--- cogs/customcom.py
+++ cogs/customcom.py
@@ -12,7 +12,7 @@ class CustomCommands:
         self.bot = bot
         self.c_commands = fileIO("data/customcom/commands.json", "load")
 
-    @commands.command(pass_context=True, no_pm=True)
+    @commands.command(pass_context=True, no_pm=True,aliases=["learn"])
     @checks.mod_or_permissions(manage_server=True)
     async def addcom(self, ctx, command : str, *text):
         """Adds a custom command
@@ -63,7 +63,7 @@ class CustomCommands:
         else:
              await self.bot.say("`There are no custom commands in this server. Use addcom [command] [text]`")
 
-    @commands.command(pass_context=True, no_pm=True)
+    @commands.command(pass_context=True, no_pm=True,aliases=["forget"])
     @checks.mod_or_permissions(manage_server=True)
     async def delcom(self, ctx, command : str):
         """Deletes a custom command
---
+++ cogs/runescape.py
@@ -0,0 +1,157 @@
+import discord
+from discord.ext import commands
+import aiohttp
+import requests
+import feedparser
+from .utils.dataIO import fileIO
+from .utils.chat_formatting import *
+import os
+import json
+
+class Runescape:
+	"""Runescape stuff"""
+	def __init__(self,bot):
+		self.bot = bot
+		self.base_url = "http://services.runescape.com/m=hiscore/index_lite.ws?player="
+		self.alog_url = "http://services.runescape.com/m=adventurers-log/rssfeed?searchName="
+		self.max_level = 120
+		self.skill_list = [
+						"Overall",
+						"Attack",
+						"Defence",
+						"Strength",
+						"Constitution",
+						"Ranged",
+						"Prayer",
+						"Magic",
+						"Cooking",
+						"Woodcutting",
+						"Fletching",
+						"Fishing",
+						"Firemaking",
+						"Crafting",
+						"Smithing",
+						"Mining",
+						"Herblore",
+						"Agility",
+						"Thieving",
+						"Slayer",
+						"Farming",
+						"Runecrafting",
+						"Hunter",
+						"Construction",
+						"Summoning",
+						"Dungeoneering",
+						"Divination",
+						"Invention"
+			 		]
+		self.elite_skills = [27,]
+		self.skill_levels = self._skill_levels()
+		self.elite_levels = [0.4796*pow(x,4)-12.788*pow(x,3)+228.56*pow(x,2)
+							 +2790.8*x-31674 for x in range(1,150)]
+
+	def _skill_levels(self):
+		xplist = []
+
+		points = 0
+		for level in range(1,self.max_level):
+			points += int(level + 300 * pow(2,level/7))
+			xplist.append(int(points/4))
+
+		return xplist
+
+	def _get_level(self,exp):
+		exp = int(exp)
+		for level,currExp in enumerate(self.skill_levels):
+			if currExp > exp:
+				return str(level+1)
+		return '120'
+
+	def _get_elite_level(self,exp):
+		exp = int(exp)
+		for level,currExp in enumerate(self.elite_levels):
+			if currExp > exp:
+				return str(level+1)
+		return '150'
+
+	def _commafy(self,num):
+		try:
+			int(num)
+		except:
+			return num
+		else:
+			return "{:,}".format(int(num))
+
+	def _fmt_hs(self,data):
+		ret = "```"
+		retlist = [['Skill','Rank','Level','Experience']]
+		for i in range(len(data)):
+			if i < len(self.skill_list):
+				splitted = data[i].split(',')
+				if i != 0:
+					if i in self.elite_skills:
+						splitted[1] = self._get_elite_level(splitted[2])
+					else:
+						splitted[1] = self._get_level(splitted[2])
+				splitted[0] = self._commafy(splitted[0])
+				splitted[1] = self._commafy(splitted[1])
+				splitted[2] = self._commafy(splitted[2])
+				retlist.append([self.skill_list[i]] + splitted)
+		col_width = max(len(word) for row in retlist for word in row) + 2
+		for row in retlist:
+		    ret += "".join(word.ljust(col_width) for word in row) + "\n"
+		ret += "```"
+		return ret
+
+	def _fmt_alog(self,username,titles):
+		ret = "```Recent Adventure Log for "+username+":\n"
+		for title in titles:
+			ret += "\t"+title+"\n"
+		ret += "```"
+		return ret
+
+	@commands.command(no_pm=True)
+	async def alog(self, username : str):
+		"""Gets a users recent adventure log"""
+		url = self.alog_url+username
+		try:
+			page = await aiohttp.get(url)
+			text = await page.text()
+			text = text.replace("\r","")
+		except:
+			await self.bot.say("No user found.")
+		
+		feed = feedparser.parse(text)
+		titles = [post.title for post in feed.entries]
+
+		await self.bot.say(self._fmt_alog(username,titles))
+
+	@commands.command(no_pm=True)
+	async def hs(self, username : str):
+		"""Gets hiscores info"""
+		url = self.base_url+username
+		try:
+			page = await aiohttp.get(url)
+			text = await page.text()
+			text = text.replace("\r","")
+			text = text.split("\n")
+		except:
+			await self.bot.say("No user found.")
+		else:
+			await self.bot.say(self._fmt_hs(text))
+
+	@commands.command()
+	async def price(self,*item_name):
+		item_name = " ".join(item_name)
+		for itemid in self.items:
+			if name.lower() in self.items.get(itemid):
+				await self.bot.say(box(self.items.get(itemid)))
+		itemid = None
+		if itemid:
+			pass
+		else:
+			await self.bot.say("!price "+item_name)
+
+def setup(bot):
+	n = Runescape(bot)
+	bot.add_cog(n)
