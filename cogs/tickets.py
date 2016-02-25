698d99bf91dfe3d15059de6bd66a9209f8fef8df
Will Tekulve <tekulvwc@mail.uc.edu>
2016-02-24 22:56:36 -0500

    added ticket per-user limiter

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -9,6 +9,11 @@ class Tickets:
     def __init__(self,bot):
         self.bot = bot
         self.tickets = fileIO("data/tickets/tickets.json","load")
+        self.settings = fileIO("data/tickets/settings.json","load")
+
+    @property
+    def ticket_limit(self):
+        return self.settings.get("TICKETS_PER_USER",0)
 
     def _get_ticket(self):
         if len(self.tickets) > 0:
@@ -21,6 +26,11 @@ class Tickets:
         else:
             return "No more tickets!"
 
+    def _get_number_tickets(self,author):
+        idnum = author.id
+        num = len([x for ticket in self.tickets for x in ticket if x == idnum])
+        return num
+
     def _add_ticket(self,author,message):
         self.tickets.append({author.id:{"name":author.name,"message":message}})
         fileIO("data/tickets/tickets.json","save",self.tickets)
@@ -28,16 +38,45 @@ class Tickets:
     @commands.command(aliases=["nt"],pass_context=True)
     @checks.mod_or_permissions(manage_messages=True)
     async def nextticket(self,ctx):
+        """Gets the next ticket"""
         await self.bot.send_message(ctx.message.author,self._get_ticket())
 
     @commands.command(pass_context=True)
-    async def ticket(self,ctx,*message):
+    async def ticket(self,ctx,*,message):
         """Adds ticket.
 
            Example: !ticket The quick brown fox? -> adds ticket"""
-        message = " ".join(message)
+        if self.ticket_limit != 0 and \
+            self._get_number_tickets(ctx.message.author) >= self.ticket_limit:
+            await self.bot.say("{}, you've reached the ticket limit!".format(ctx.message.author.mention))
+            return
         self._add_ticket(ctx.message.author,message)
-        await self.bot.say("Ticket added.")
+        await self.bot.say("{}, ticket added.".format(ctx.message.author.mention))
+
+    @commands.command(aliases=['ct'])
+    @checks.mod_or_permissions(manage_messages=True)
+    async def cleartickets(self):
+        """Clears all tickets"""
+        self.tickets = []
+        fileIO("data/tickets/tickets.json","save",self.tickets)
+        await self.bot.say("Tickets cleared.")
+
+    @commands.group(pass_context=True)
+    @checks.admin_or_permissions(manage_server=True)
+    async def ticketset(self,ctx):
+        """Ticket cog settings"""
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @ticketset.command(name="limit")
+    async def tickets_per_user(self,num : int):
+        """Limits the number of tickets a user can have."""
+        if num < 0:
+            send_cmd_help(self.tickets_per_user)
+            return
+        self.settings["TICKETS_PER_USER"] = num
+        fileIO("data/tickets/settings.json","save",self.settings)
+        await self.bot.say("Tickets per user set to {}".format(num))
 
 def check_folder():
     if not os.path.exists("data/tickets"):
@@ -46,11 +85,17 @@ def check_folder():
 
 def check_file():
     tickets = []
+    settings = {"TICKETS_PER_USER":0,"REPLY_TO_CHANNEL":False}
 
     f = "data/tickets/tickets.json"
     if not fileIO(f, "check"):
         print("Creating default tickets's tickets.json...")
         fileIO(f, "save", tickets)
+
+    f = "data/tickets/settings.json"
+    if not fileIO(f, "check"):
+        print("Creating default tickets's settings.json...")
+        fileIO(f, "save", settings)
 
 def setup(bot):
     check_folder()
