7d40c22d89dabb5153492c68be4b73e8ea8a1d5f
Will Tekulve <tekulvwc@mail.uc.edu>
2016-02-25 20:35:45 -0500

    add time limiter

--- cogs/mentiontracker.py
+++ cogs/mentiontracker.py
@@ -6,11 +6,30 @@ from .utils.dataIO import fileIO
 from .utils import checks
 from __main__ import send_cmd_help
 import os
+import datetime
 
 class MentionTracker:
     def __init__(self,bot):
         self.bot = bot
         self.mail = fileIO("data/mentiontracker/mail.json","load")
+        self.settings = fileIO("data/mentiontracker/settings.json","load")
+
+    @commands.group(pass_context=True)
+    @checks.mod_or_permissions(manage_messages=True)
+    async def mentionset(self,ctx):
+        """Manage mentiontracker settings"""
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @mentionset.command(name="limit",pass_context=True)
+    async def _mentionset_limit(self,ctx,num : int):
+        """Number of minutes to wait in between saving mentions."""
+        if num < 0:
+            send_cmd_help(ctx)
+            return
+        self.settings["MENTION_TIME_LIMIT"] = num
+        fileIO("data/mentiontracker/settings.json","save",self.settings)
+        self.bot.say("Settings saved.")
 
     @commands.group(pass_context=True)
     async def mention(self,ctx):
@@ -25,15 +44,16 @@ class MentionTracker:
         if user.id not in self.mail:
             self.mail[user.id] = []
             fileIO("data/mentiontracker/mail.json","save",self.mail)
+            await self.bot.reply("thanks for registering.")
         else:
-            await self.bot.reply("You're already registered.")
+            await self.bot.reply("you're already registered.")
 
     @mention.command(pass_context=True,name="read")
     async def _mention_read(self,ctx):
         """Read all mentions since you've been away."""
         user = ctx.message.author
         if user.id not in self.mail:
-            await self.bot.reply("You're not registered!")
+            await self.bot.reply("you're not registered!")
             temp_context = ctx
             temp_context.invoked_subcommand = self._mention_register
             await send_cmd_help(temp_context)
@@ -61,20 +81,37 @@ class MentionTracker:
     def _add_mail(self,add_id,message):
         mail = {}
         mail["author"] = message.author.name
-        mail["message"] = message.content
+        mail["message"] = self._clean_message(message)
         mail["server"] = message.server.name
         mail["channel"] = message.channel.name
         mail["time"] = str(message.timestamp)
         self.mail[add_id].append(mail)
         fileIO("data/mentiontracker/mail.json","save",self.mail)
 
+    def _clean_message(self,message):
+        ret = message.content
+        for user in message.mentions:
+            ret = ret.replace(user.mention,"@"+user.name)
+        return ret
+
+    def _last_time(self,mention):
+        mail = self.mail[mention.id]
+        if len(mail) > 0:
+            last_mention = mail[-1]
+            return datetime.datetime.strptime(last_mention["time"],"%Y-%m-%d %H:%M:%S.%f")
+        else:
+            return datetime.datetime.min
+
     async def tracker(self,message):
         if message.author == self.bot.user:
             return
         mentions = message.mentions
         for mention in mentions:
-            if mention != message.author and mention.id in self.mail and message.author.status != 'online':
-                self._add_mail(mention.id,message)
+            if mention != message.author and mention.id in self.mail:# and mention.status != 'online':
+                limit = self.settings.get("MENTION_TIME_LIMIT",0)
+                delta = datetime.timedelta(minutes=limit)
+                if self._last_time(mention) + delta < datetime.datetime.utcnow():
+                    self._add_mail(mention.id,message)
 
 def check_folder():
     if not os.path.exists("data/mentiontracker"):
@@ -83,11 +120,17 @@ def check_folder():
 
 def check_file():
     mail = {}
+    settings = {"MENTION_TIME_LIMIT":5}
 
     f = "data/mentiontracker/mail.json"
     if not fileIO(f, "check"):
         print("Creating default mentiontracker's mail.json...")
         fileIO(f, "save", mail)
+
+    f = "data/mentiontracker/settings.json"
+    if not fileIO(f, "check"):
+        print("Creating default mentiontracker's settings.json...")
+        fileIO(f, "save", settings)
 
 def setup(bot):
     check_folder()
