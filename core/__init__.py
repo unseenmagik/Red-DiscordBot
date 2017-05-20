464d3eee5ce88c4e4bea675cf4beacbedf9f648c
Will Tekulve <tekulve.will@gmail.com>
2017-05-20 14:52:25 -0700

    Modify core/__init__.py to reduce some naming conventions

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+from core.bot import Red, ExitCodes
+from core.core_commands import Core
+from core.dev_commands import Dev
+from core.json_io import JsonIO
+from core.settings import CoreDB, parse_cli_flags
